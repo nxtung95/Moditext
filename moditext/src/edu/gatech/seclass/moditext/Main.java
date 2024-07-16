738207408bc5d43a37ed47750f4efa12f61e3817
tungnx <nxtung95@gmail.com>
2024-07-16 22:37:57 +0700

    Update

--- moditext/src/edu/gatech/seclass/moditext/Main.java
+++ moditext/src/edu/gatech/seclass/moditext/Main.java
@@ -1,11 +1,14 @@
 package edu.gatech.seclass.moditext;
 
 import java.io.File;
-import java.nio.file.*;
-import java.nio.file.Path;
 import java.io.IOException;
 import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.*;
+import java.util.stream.Collectors;
 
 //public class Main {
 //    // Empty Main class for compiling Individual Project
@@ -57,18 +60,14 @@ public class Main {
         }
 
         List<String> lines;
-        String fileContent = "";
         try {
-            lines = Files.readAllLines(filePath, Charset.forName("UTF-8"));
-            fileContent = Files.readString(filePath, Charset.forName("UTF-8"));
+            lines = Files.readAllLines(filePath, StandardCharsets.UTF_8);
         } catch (IOException e) {
             e.printStackTrace();
             return;
         }
 
         String[] validOptions = new String[] {"-k", "-p", "-t", "-g", "-f", "-r"};
-//        boolean filterApplied = false;
-        List<String> filteredLines = new ArrayList<>(lines);
 
         Map<String, String> optionMap = new HashMap<>();
         for (int i = 0; i < args.length - 1; i++) {
@@ -84,22 +83,12 @@ public class Main {
                 optionMap.put(option, "");
             } else if ("-p".equals(option) || "-f".equals(option)) {
                 try {
-                    String nextOpt = args[i + 1];
-                    if (Arrays.asList(validOptions).contains(nextOpt)) {
-                        usage();
-                        return;
-                    }
-                    value1 = nextOpt;
+                    value1 = args[i + 1];
                 } catch (Exception e) {
                     return;
                 }
                 try {
-                    String nextOpt = args[i + 2];
-                    if (Arrays.asList(validOptions).contains(nextOpt)) {
-                        usage();
-                        return;
-                    }
-                    value2 = nextOpt;
+                    value2 = args[i + 2];
                 } catch (Exception e) {
                     return;
                 }
@@ -107,12 +96,7 @@ public class Main {
                 i = i + 2;
             } else {
                 try {
-                    String nextOpt = args[i + 1];
-                    if (Arrays.asList(validOptions).contains(nextOpt)) {
-                        usage();
-                        return;
-                    }
-                    value1 = nextOpt;
+                    value1 = args[i + 1];
                 } catch (Exception e) {
                     return;
                 }
@@ -130,13 +114,7 @@ public class Main {
             return;
         }
 
-        if (fileContent.length() < 2) {
-            return;
-        } else if ((System.lineSeparator().length() == 2 && !fileContent.substring(fileContent.length() - 2).equals(System.lineSeparator()))) {
-            usage();
-            return;
-        } else if ((System.lineSeparator().length() == 1 && !fileContent.substring(fileContent.length() - 1).equals(System.lineSeparator()))) {
-            usage();
+        if (lines.isEmpty()) {
             return;
         }
 
@@ -146,41 +124,8 @@ public class Main {
         }
 
         if (keys.contains("-p")) {
-            String[] value = optionMap.get("-p").split("&&@@!!");
-            String cha = value[0];
-            String num = value[1];
-
-            if (cha.length() > 1 && !Character.isLetter(cha.charAt(0))) {
-                usage();
-                return;
-            }
-
-            int padNum;
-            try {
-                padNum = Integer.parseInt(num);
-            } catch (Exception e) {
-                usage();
-                return;
-            }
-
-            if (padNum < 1 || padNum > 100) {
-                usage();
-                return;
-            }
             sortMap.put("-p", optionMap.get("-p"));
         } else if (keys.contains("-t")) {
-            String num = optionMap.get("-t");
-            int trimNum;
-            try {
-                trimNum = Integer.parseInt(num);
-            } catch (Exception e) {
-                usage();
-                return;
-            }
-            if (trimNum < 0 || trimNum > 100) {
-                usage();
-                return;
-            }
             sortMap.put("-t", optionMap.get("-t"));
         }
 
@@ -196,101 +141,147 @@ public class Main {
             sortMap.put("-r", optionMap.get("-r"));
         }
 
-//            switch (option) {
-//                case "-k":
-//                    if (i + 1 >= args.length - 1) {
-//                        usage();
-//                        return;
-//                    }
-//                    String substring = args[++i];
-//                    filteredLines.removeIf(line -> !line.contains(substring));
-//                    filterApplied = true;
-//                    break;
-//
-//                case "-p":
-//                    if (i + 2 >= args.length - 1) {
-//                        usage();
-//                        return;
-//                    }
-//                    String symbol = args[++i];
-//                    int maxPadding = Integer.parseInt(args[++i]);
-//                    if (maxPadding < 1 || maxPadding > 100) {
-//                        usage();
-//                        return;
-//                    }
-//                    for (int j = 0; j < filteredLines.size(); j++) {
-//                        String line = filteredLines.get(j);
-//                        if (line.length() < maxPadding) {
-//                            line = symbol.repeat(maxPadding - line.length()) + line;
-//                        }
-//                        filteredLines.set(j, line);
-//                    }
-//                    break;
-//
-//                case "-t":
-//                    if (i + 1 >= args.length - 1) {
-//                        usage();
-//                        return;
-//                    }
-//                    int num = Integer.parseInt(args[++i]);
-//                    if (num < 0 || num > 100) {
-//                        usage();
-//                        return;
-//                    }
-//                    for (int j = 0; j < filteredLines.size(); j++) {
-//                        String line = filteredLines.get(j);
-//                        if (line.length() > num) {
-//                            line = line.substring(0, num);
-//                        }
-//                        filteredLines.set(j, line);
-//                    }
-//                    break;
-//
-//                case "-f":
-//                    if (i + 2 >= args.length - 1) {
-//                        usage();
-//                        return;
-//                    }
-//                    String style = args[++i];
-//                    substring = args[++i];
-//                    for (int j = 0; j < filteredLines.size(); j++) {
-//                        String line = filteredLines.get(j);
-//                        switch (style) {
-//                            case "bold":
-//                                line = line.replaceFirst(substring, "**" + substring + "**");
-//                                break;
-//                            case "italic":
-//                                line = line.replaceFirst(substring, "*" + substring + "*");
-//                                break;
-//                            case "code":
-//                                line = line.replaceFirst(substring, "`" + substring + "`");
-//                                break;
-//                            default:
-//                                usage();
-//                                return;
-//                        }
-//                        filteredLines.set(j, line);
-//                    }
-//                    break;
-//
-//                case "-r":
-//                    Collections.reverse(filteredLines);
-//                    break;
-//
-//                default:
-//                    usage();
-//                    return;
-//            }
-//        }
+        List<String> filteredLines = new ArrayList<>(lines);
+        filteredLines = filteredLines.stream().map(f -> f + System.lineSeparator()).collect(Collectors.toList());
+        for (Map.Entry<String, String> entry : sortMap.entrySet()) {
+            String key = entry.getKey();
+            String value = entry.getValue();
 
-//        if (filterApplied && filteredLines.isEmpty()) {
-//            // No output if filter was applied and no lines matched
-//            return;
-//        }
+            if ("-k".equals(key)) {
+                filteredLines = getKeepLineContainValue(value, filteredLines);
+            }
+
+            if ("-p".equals(key)) {
+                String[] padding = optionMap.get("-p").split("&&@@!!");
+                String cha = padding[0];
+                String num = padding[1];
+
+                if (cha.length() != 1) {
+                    usage();
+                    return;
+                }
+
+                int padNum;
+                try {
+                    padNum = Integer.parseInt(num);
+                } catch (Exception e) {
+                    usage();
+                    return;
+                }
+
+                if (padNum < 1 || padNum > 100) {
+                    usage();
+                    return;
+                }
+
+                filteredLines = getPaddingLines(filteredLines, cha, padNum);
+            } else if ("-t".equals(key)) {
+                String num = optionMap.get("-t");
+                int trimNum;
+                try {
+                    trimNum = Integer.parseInt(num);
+                } catch (Exception e) {
+                    usage();
+                    return;
+                }
+                if (trimNum < 0 || trimNum > 100) {
+                    usage();
+                    return;
+                }
+                filteredLines = getTrimLines(filteredLines, trimNum);
+            }
+
+            if ("-f".equals(key)) {
+                String[] formatText = optionMap.get("-f").split("&&@@!!");
+                String style = formatText[0];
+                String substring = formatText[1];
+                if (!Arrays.asList("bold", "italic", "code").contains(style)) {
+                    usage();
+                    return;
+                } else if (substring.isEmpty()) {
+                    usage();
+                    return;
+                }
+                boolean isGlobal = keys.contains("-g");
+                filteredLines = getFormatText(filteredLines, style, substring, isGlobal);
+            }
+
+            if ("-r".equals(key)) {
+                Collections.reverse(filteredLines);
+            }
+        }
 
         for (String line : filteredLines) {
-            System.out.println(line);
+            System.out.print(line);
         }
+    }
+
+    private static List<String> getFormatText(List<String> filteredLines, String style, String substring, boolean isGlobal) {
+        List<String> result = new ArrayList<>();
+        String special;
+        if ("bold".equals(style)) {
+            special = "**";
+        } else if ("italic".equals(style)) {
+            special = "*";
+        } else {
+            special = "`";
+        }
+        for (String line : filteredLines) {
+            String newLine = line;
+            if (line.contains(substring)) {
+                if (isGlobal) {
+                    newLine = line.replaceAll(substring, special + substring + special);
+                } else {
+                    newLine = line.replaceFirst(substring, special + substring + special);
+                }
+            }
+            result.add(newLine);
+        }
+        return result;
+    }
+
+    private static List<String> getTrimLines(List<String> filteredLines, int trimNum) {
+        List<String> result = new ArrayList<>();
+        for (String line : filteredLines) {
+            String formatLine = line.substring(0, line.length() - System.lineSeparator().length());
+            if (formatLine.length() > trimNum) {
+                formatLine = formatLine.substring(0, trimNum);
+            } else if (formatLine.length() < trimNum) {
+                formatLine = "";
+            }
+            result.add(formatLine + System.lineSeparator());
+        }
+        return result;
+    }
+
+    private static List<String> getPaddingLines(List<String> filteredLines, String cha, int padNum) {
+        List<String> result = new ArrayList<>();
+        for (String line : filteredLines) {
+            String formatLine = line.substring(0, line.length() - System.lineSeparator().length());
+            if (formatLine.length() < padNum) {
+                int addPadNum = padNum - formatLine.length();
+                StringBuilder lineBuilder = new StringBuilder(line);
+                for (int i = 0; i < addPadNum; i++) {
+                    lineBuilder.insert(0, cha);
+                }
+                line = lineBuilder.toString();
+            }
+            result.add(line);
+        }
+        return result;
+    }
+
+    private static List<String> getKeepLineContainValue(String value, List<String> lines) {
+        if (value.isEmpty()) {
+            return lines;
+        }
+        List<String> keepLines = new ArrayList<>();
+        for (String line : lines) {
+            if (line.contains(value)) {
+                keepLines.add(line);
+            }
+        }
+        return keepLines;
     }
 
     private static void usage() {
